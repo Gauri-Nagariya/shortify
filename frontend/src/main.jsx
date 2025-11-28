5f36088c5448b829ce88c66b9fba0137429833b9
Gauri Nagariya <gaurinagariya@gmail.com>
2025-11-28 12:06:10 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,10 @@
-// import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App.jsx'
 import AppContextProvider from "./context/AppContext";
 import { BrowserRouter } from "react-router-dom";
-// import { CompanyContext } from './context/CompanyContext.jsx';
-// import CompanyContextProvider from "./context/CompanyContext.jsx";
 import UrlContextProvider from './context/UrlContext.jsx';
-
 
 createRoot(document.getElementById('root')).render(
 <AppContextProvider>
